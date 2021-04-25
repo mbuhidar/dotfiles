447851a2f466891dcb6ab453fe56bc8994042845
mbuhidar <mbuhidar@gmail.com>
2021-04-25 10:57:29 -0400

    Change format of dotfiles in dotfiles directory to have a dot in front.

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_bak             # old dotfiles backup directory
-files="bashrc bash_aliases"    # list of files/folders to symlink in homedir
+files=".bashrc .bash_aliases"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -25,7 +25,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_bak directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_bak/
+    mv ~/$file ~/dotfiles_bak/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/$file ~/$file
 done
